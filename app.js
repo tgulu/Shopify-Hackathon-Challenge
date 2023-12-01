7fb9dd47a5b867d5cca33413239ade4d0a6c1194
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2023-12-01 00:03:46 +0000

    added dropdown menu for alerts and admin user

--- app.js
+++ app.js
@@ -18,3 +18,11 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+
+let subMenu = document.getElementById("subMenu");
+
+function toggleMenu(){
+    subMenu.classList.toggle('hidden');
+}
